ebe06727278e3ad1d4df77a4ebbc6885166bf826
veliaky <veli@aky-tech.com>
2022-08-13 15:13:09 +0300

    Webcam Acildi 15:12

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {useRef, useEffect} from "react";
+import * as stream from "stream";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const video = useRef(null);
+
+    useEffect(() => {
+        navigator.mediaDevices.getUserMedia({video: {width: 1280, height: 720}})
+            .then(stream => {
+                video.current.srcObject = stream;
+                video.current.play();
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+
+    return (
+        <>
+            <video ref={video} autoPlay muted/>
+        </>
+    );
 }
 
 export default App;
